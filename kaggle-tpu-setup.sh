222af7bbe53e77f879fc00d7281a9d8ed32eaa4e
Quasar-Kim <vhfflrhs128@gmail.com>
2023-05-08 01:36:41 +0900

    setup 스크립트 수정

--- kaggle-tpu-setup.sh
+++
@@ -1,2 +0,0 @@
-unset PJRT_DEVICE
-XRT_TPU_COFIG="localservice;0;localhost:51011"
--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install -y software-properties-common
+sudo apt-get update -y
+sudo apt-get upgrade -y
 sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt-get install -y software-properties-common python3.11
 curl -sSL https://install.python-poetry.org | python3 -
-PATH="~/.local/bin:$PATH"
-poetry env use $(which python3.11)
-poetry install --no-root
+~/.local/bin/poetry env use $(which python3.11)
+~/.local/bin/poetry install -q --no-root
