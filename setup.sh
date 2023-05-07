20fd3c4cebeeb27a616aaa26abf121c8a73c0a12
Quasar-Kim <vhfflrhs128@gmail.com>
2023-05-08 02:30:22 +0900

    Remove upgrade

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 sudo apt-get update -y
-sudo apt-get upgrade -y
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get install -y software-properties-common python3.11
 curl -sSL https://install.python-poetry.org | python3 -
