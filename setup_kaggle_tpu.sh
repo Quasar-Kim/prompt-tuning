e702f2744808014379e29969a837ef42d77d8ad6
Quasar-Kim <vhfflrhs128@gmail.com>
2023-05-08 11:51:39 +0900

    Fix typo

--- setup_kaggle_tpu.sh
+++ setup_kaggle_tpu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -sSL https://install.python-poetry.org | python3 -
-!~/.local/bin/poetry export -f requirements.txt > requirements.txt
-!pip install -r requirements.txt
+~/.local/bin/poetry export -f requirements.txt > requirements.txt
+pip install -r requirements.txt
