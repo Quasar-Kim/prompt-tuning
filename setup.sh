0046efc508683e0a70e9bfcb04b0377eb518fe8e
Quasar-Kim <vhfflrhs128@gmail.com>
2023-05-10 02:06:42 +0900

    Prepare tpu support

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-sudo apt-get install -y python3.8
+if command -v "sudo" >/dev/null 2>&1; then
+    sudo apt-get install -y python3.8
+else
+    apt-get install -y python3.8
+fi
 curl -sSL https://install.python-poetry.org | python3 -
-~/.local/bin/poetry config --local virtualenvs.in-project true
 ~/.local/bin/poetry env use $(which python3.8)
 ~/.local/bin/poetry install -q --no-root
