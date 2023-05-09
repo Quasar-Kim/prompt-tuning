81e78808d355c517036d1e8c3ac304509958feea
Quasar-Kim <vhfflrhs128@gmail.com>
2023-05-09 13:45:48 +0900

    Revise setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt-get install -y python3.10
+sudo apt-get install -y python3.8
 curl -sSL https://install.python-poetry.org | python3 -
 ~/.local/bin/poetry config --local virtualenvs.in-project true
-~/.local/bin/poetry env use $(which python3.10)
+~/.local/bin/poetry env use $(which python3.8)
 ~/.local/bin/poetry install -q --no-root
