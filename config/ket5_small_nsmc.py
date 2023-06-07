586575d22c5f42f955660b0e929deb6f52892cb5
Quasar-Kim <vhfflrhs128@gmail.com>
2023-06-07 23:10:50 +0900

    Lower ket5_small_nsmc batch size to 16

--- config/ket5_small_nsmc.py
+++ config/ket5_small_nsmc.py
@@ -3,5 +3,5 @@ from model import ket5_small
 
 def config(cfg: dict):
     cfg["model"] = ket5_small
-    cfg["runtime_config"]["batch_size"] = 32
+    cfg["runtime_config"]["batch_size"] = 16
     return cfg
